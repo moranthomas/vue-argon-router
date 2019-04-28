bc5479a915a962e7e4947283a6998695ebd59c6b
moranthomas <thomas@optimizme.com>
2019-04-28 16:06:35 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,83 @@
+<template>
+  <div id="app">
+    <div id="nav">
+
+      <section class="">
+        <!-- Navbar default -->
+        <base-nav v-for="menu in menus"
+                  :key="menu.type"
+                  :type="menu.type"
+                  effect="dark"
+                  expand
+                  title="Brand Logo"
+                  class="mt-4 lead text-white"
+                  :content-id="`navbar-${menu.type}`">
+            <div class="row" slot="content-header" slot-scope="{closeMenu}">
+                <div class="col-6 collapse-brand">
+                    <a href="./index.html">
+                        <img src="img/brand/blue.png">
+                    </a>
+                </div>
+                <div class="col-6 collapse-close">
+                    <close-button @click="closeMenu" :target="`navbar-${menu.type}`">
+
+                    </close-button>
+                </div>
+            </div>
+            <component :is="menu.menuComponent"></component>
+        </base-nav>
+    </section>
+
+
+
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import BaseNav from "@/components/BaseNav";
+import CloseButton from "@/components/CloseButton";
+import NavMenu from "./views/NavMenu";
+
+export default {
+  components: {
+    BaseNav,
+    CloseButton,
+    NavMenu
+  },
+  data() {
+    return {
+      menus: [
+        { type: "default", menuComponent: NavMenu },
+        //{ type: "primary", menuComponent: Menu2 },
+        //{ type: "danger", menuComponent: Menu2 },
+        //{ type: "info", menuComponent: Menu2 }
+      ]
+    };
+  }
+};
+</script>
+
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  margin: 0px 30px 30px 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  /*color: #2c3e50;*/
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/views/NavMenu.vue
@@ -0,0 +1,67 @@
+<template>
+    <ul class="navbar-nav ml-lg-auto">
+
+        <li class="nav-item">
+            <router-link class="nav-link" to="/">Home</router-link>
+        </li>
+        <li class="nav-item">
+            <router-link class="nav-link" to="/about">About</router-link>
+        </li>
+
+
+        <li class="nav-item">
+            <a class="nav-link" href="#">Discover
+                <span class="sr-only">(current)</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Profile</a>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link" href="#" id="navbar-primary_dropdown_1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Settings</a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbar-primary_dropdown_1">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+        </li>
+
+         <li class="nav-item">
+            <a class="nav-link nav-link-icon" href="#">
+                <i class="fa fa-facebook-square"></i>
+                <span class="nav-link-inner--text d-lg-none">Share</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link nav-link-icon" href="#">
+                <i class="fa fa-twitter"></i>
+                <span class="nav-link-inner--text d-lg-none">Tweet</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link nav-link-icon" href="#">
+                <i class="fa fa-pinterest"></i>
+                <span class="nav-link-inner--text d-lg-none">Pin</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link nav-link-icon" href="#">
+                <i class="fa fa-google-plus"></i>
+                <span class="nav-link-inner--text d-lg-none">Google +</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link nav-link-icon" href="#">
+                <i class="fa fa-instagram"></i>
+                <span class="nav-link-inner--text d-lg-none">Instagram</span>
+            </a>
+        </li>
+
+    </ul>
+</template>
+<script>
+export default {};
+</script>
+<style>
+</style>
