c5cd1845a2fc1db12b1751e245c2881e1b6450ff
moranthomas <thomas@optimizme.com>
2019-05-17 13:37:00 +0100

    all except transfers working wit Web3js

--- src/views/Transactions.vue
+++ src/views/Transactions.vue
@@ -1,28 +1,38 @@
 <template>
     <div class="container">
 
-        	<h1>{{ message }}</h1>
-        	<button @click="clickedButton">Click Me!</button>
-
-        <h1>Web3.js</h1>
-
-        <label for="name" class="col-lg-2 control-label"><h3>NodeInfo</h3></label>
-        <input id="NodeInfo" type="text">
-
+        <h2> Account Transactions (Web3.js) </h2>
+        <hr>
+        <label for="name" class="col-lg-2 control-label"><h4> Node</h4></label>
+        <button  @click="getNodeInfoClicked">Get Node Info</button>
+        <p>Node Info :  <input class="node-input" ref="NodeInfoInputField" id="NodeInfo" type="text"> </p>
 		<hr>
 
-        <label for="name" class="col-lg-2 control-label"><h3>Balance</h3></label>
-		<p>Account : <input id="Account" type="text"> </p>
-        <p>Balance : <input id="Balance" type="text"></p>
-		 <button id="checkBalance">Check Balance</button>
+
+        <label for="name" class="col-lg-2 control-label"><h4>Balance</h4></label>
+        <button ref="popAccounts" id="popAccounts" @click="populateAccountsClicked">Populate Accounts</button>
+
+        <span>Choose Account : {{ selected }} </span><span>&nbsp;</span>
+        <select id="accountsDropdown" ref="accountsDropdownRef" v-model="selected" v-on:change="onChange">
+            <option v-for="(item, key) in accounts" :value="key">
+                {{item}}
+            </option>
+        </select>
+        </br>
+        </br>
+		<p>Selected Account : <input id="Account" ref="accountRef" type="text"> </input>
+        <div>
+            <p>Balance : <input id="Balance" ref="balanceRef" type="text"></p>
+		    <button ref="checkBalance" id="checkBalance" @click="checkBalanceClicked">Check Balance</button>
+        </div>
 
 		<hr>
-		<label for="name" class="col-lg-2 control-label"><h3>Transfer</h3></label>
-		<p>From : &nbsp &nbsp &nbsp <input id="From" type="text"> </p>
-		<p>To : &nbsp &nbsp &nbsp &nbsp &nbsp <input id="To" type="text"> </p>
-        <p>Amount : &nbsp <input id="Amount" type="text"></p>
+		<label for="name" class="col-lg-2 control-label"><h4>Transfer</h4></label>
+		<p>From:<input id="From" type="text"> </p>
+		<p>To:<input id="To" type="text"> </p>
+        <p>Amount:<input id="Amount" type="text"></p>
         <button id="Transfer">Transfer</button>
-        <p>Transaction Hash : &nbsp  <span id="Tx"></span></p>
+        <p>Transaction Hash :<span id="Tx"></span></p>
 
     </div>
 
@@ -30,107 +40,143 @@
 
 
 <script>
-    export default {
-        methods: {
-            clickedButton: function() {
-                 if (typeof web3 !== 'undefined') {
-                    web3 = new Web3(web3.currentProvider);
-                } else {
-                        // set the provider you want from Web3.providers
-                    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-                }
-                web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
-                /* Get Node Info */
-                web3.eth.getNodeInfo(function(error, result){
-                    if(error){
-                        console.log( "error" ,error);
+    import Web3 from 'web3';
+
+    let web3;
+    let web3Provider;
+
+    export default {
+        data () {
+            return {
+                selected: '0',
+                accounts: []
+            }
+        },
+        mounted() {
+            this.loadWeb3Provider();
+        },
+        methods: {
+
+            loadWeb3Provider: function () {
+
+                const infuraUrl = "http://mainnet.infura.io/v3/53dbf207e63c42e99cacb63c2d41ec4f";
+                const ganacheUrl = "http://localhost:7545";
+
+                const local = true;
+                if(local) {
+                    web3Provider = new Web3.providers.HttpProvider(ganacheUrl);
+                }
+                else {
+                    // Modern dapp browsers...
+                    if (window.ethereum) {
+                        web3Provider = window.ethereum;
+                        try {
+                            // Request account access
+                            window.ethereum.enable();
+                        } catch (error) {
+                            // User denied account access...
+                            console.error("User denied account access")
+                        }
+                    }
+                    // Legacy dapp browsers...
+                    else if (window.web3) {
+                        web3Provider = window.web3.currentProvider;
+                    }
+                    // If no injected web3 instance is detected, fall back to Ganache
+                    else {
+                        web3Provider = new Web3.providers.HttpProvider(ganacheUrl);
+                    }
+                }
+
+                web3 = new Web3(web3Provider);
+
+            },
+            getNodeInfoClicked: function() {
+                /* Get Node Info*/
+                this.$refs.NodeInfoInputField.value = web3.currentProvider.host
+            },
+            populateAccountsClicked: function(event) {
+                let self = this       // to get around lexical scoping issue - could use another outer arrow fn instead
+                web3.eth.getAccounts(function(error, accounts) {
+                    if(error) {
+                        console.log(error);
                     }
                     else{
-                        console.log( "result",result );
-                        $('#NodeInfo').val(result);
+                        self.accounts = accounts
+                        for(var account of accounts) {
+                            console.log(self.$refs.accountRef.value)
+                        }
                     }
                 });
-
+            },
+            checkBalanceClicked: function() {
+               //Get Balance
+               let balanceinEther;
+               let self = this       // to get around lexical scoping issue - could use another outer arrow fn instead
+               let selectedAccount = this.$refs.accountRef.value
+               web3.eth.getBalance(selectedAccount).then(result => {
+                    balanceinEther = web3.utils.fromWei(result, 'ether');
+                    self.$refs.balanceRef.value = balanceinEther;
+               });
+            },
+            onChange: function (event) {
+                var index = event.srcElement.value;
+                this.$refs.accountRef.value = event.srcElement[index].label
             }
         }
     }
+
 </script>
 
-<!--
- <script>
 
-    $( document ).ready(function() {
-        console.log( "ready!" );
+<style>
+body {
+    background-color:#F0F0F0;
+    padding: 2em;
+    font-family: 'Raleway','Source Sans Pro', 'Arial';
+}
+.container {
+    width: 50%;
+    margin: 0 auto;
+}
 
-        if (typeof web3 !== 'undefined') {
-            web3 = new Web3(web3.currentProvider);
-        } else {
-                // set the provider you want from Web3.providers
-            web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-        }
-        web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-        /* Get Node Info */
-        web3.eth.getNodeInfo(function(error, result){
-            if(error){
-                console.log( "error" ,error);
-            }
-            else{
-                console.log( "result",result );
-                $('#NodeInfo').val(result);
-            }
-        });
+/*label{
+    display: inline-block;
+    float: left;
+    clear: left;
+    width: 250px;
+    text-align: right;
+}
+input {
+  display: inline-block;
+  float: left;
+}*/
 
-        /*Get Balance */
-        web3.eth.getAccounts(function(error, accounts) {
-            if(error) {
-                console.log(error);
-            }
-            $('#Account').val(accounts[0]);
-            web3.eth.getBalance(accounts[0]).then(function(result){
-                console.log( "Balance : " ,web3.utils.fromWei(result, 'ether'));
-                $('#Balance').val(web3.utils.fromWei(result, 'ether'));
-            });
-        });
+label {
+    display: block;
+    margin-bottom:10px;
+    margin-right: 20px;
+    float: left;
+}
+input {
+    padding:10px;
+    width: 50%;
+    margin-bottom: 1em;
+}
+.node-input{
+    width: 70%;
+}
 
-        $('#checkBalance').click(function() {
-            var _account = $('#Account').val();
-            web3.eth.getBalance(_account).then(function(result){
-                console.log( "Balance : " ,web3.utils.fromWei(result, 'ether'));
-                $('#Balance').val(web3.utils.fromWei(result, 'ether'));
-            });
-        });
+button {
+    margin: 2em 0;
+    padding: 1em 4em;
+    display:block;
+}
 
-
-        /* Transfer */
-        $('#Transfer').click(function() {
-            $('#Tx').text('');
-            var _from = $('#From').val();
-            var _to = $('#To').val();
-            var _Amount = $('#Amount').val();
-            var txnObject = {
-                "from":_from,
-                "to": _to,
-                "value": web3.utils.toWei(_Amount,'ether'),
-                // "gas": 21000,         (optional)
-                // "gasPrice": 4500000,  (optional)
-                // "data": 'For testing' (optional)
-                // "nonce": 10           (optional)
-            }
-
-            web3.eth.sendTransaction(txnObject, function(error, result){
-                if(error){
-                    console.log( "Transaction error" ,error);
-                }
-                else{
-                    var txn_hash = result; //Get transaction hash
-                    $('#Tx').text(txn_hash);
-                }
-            });
-
-        });
-
-
-    });
-
-</script>-->
+#instructor {
+    padding:1em;
+    background-color:#fff;
+    margin: 1em 0;
+}
+</style>
